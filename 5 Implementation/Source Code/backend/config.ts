cb38ac76f78c01efa495f22b47eba998dfe3f15f
cloycloe <chloedanemabanding@gmail.com>
2025-05-28 17:13:41 +0800

    Add files via upload

---
+++ 5 Implementation/Source Code/backend/config.ts
@@ -0,0 +1,8 @@
+export const API_CONFIG = {
+  baseURL: 'http://192.168.5.178:3000/api',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  }
+}; 
